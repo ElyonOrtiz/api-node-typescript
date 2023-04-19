23b46701baa127ce6ccb7ae15d4575c4b66a9d3c
elyon.ortiz <elyon.ortiz08@gamil.com>
2023-04-19 13:21:19 -0400

    adicionando e aprendendo a usar o yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,6 +1,28 @@
-import { Request, Response } from 'express';
+import { Request, Response}  from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
+interface ICidade{
+  nome: string;
+}
 
-export const create = (req: Request, res:Response) => {
-  return res.send('Create!');
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+export const create = async(req: Request< {}, {}, ICidade >, res:Response) => {
+  let validateData: ICidade | undefined = undefined;
+  
+  try{
+    validateData = await bodyValidation.validate(req.body);
+  }catch(error){
+    const yupError = error as yup.ValidationError;
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+  
+  return res.send(validateData);
 };
