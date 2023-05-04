0267bec0c4fd91423aba658ba181cfdc4fa1fcb5
elyon.ortiz <elyon.ortiz08@gamil.com>
2023-05-04 09:10:55 -0400

    mesmo commit anterior

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,4 +1,5 @@
-import { Request, Response }  from 'express';
+
+import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
@@ -6,23 +7,25 @@ import { CidadesProvider } from '../../database/providers/cidades';
 import { validation } from '../../shared/middleware';
 import { ICidade } from '../../database/models';
 
+
 interface IBodyProps extends Omit<ICidade, 'id'> { }
 
-export const createValidation = validation((getschema) => ({
-  body: getschema<IBodyProps>(yup.object().shape({
-    nome: yup.string().required().min(3),
-  })),  
-  
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<IBodyProps>(yup.object().shape({
+    nome: yup.string().required().min(3).max(150),
+  })),
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   const result = await CidadesProvider.create(req.body);
-  if(result instanceof Error){
+
+  if (result instanceof Error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors:{
+      errors: {
         default: result.message
       }
     });
   }
+
   return res.status(StatusCodes.CREATED).json(result);
-};  
+};
--- src/server/shared/middleware/Validation.ts
+++ src/server/shared/middleware/Validation.ts
@@ -1,48 +1,43 @@
 import { RequestHandler } from 'express';
-import { StatusCodes } from 'http-status-codes';
 import { AnyObject, Maybe, ObjectSchema, ValidationError } from 'yup';
+import { StatusCodes } from 'http-status-codes';
 
-type TProperty = 'body'  | 'header' | 'params' | 'query' ;
+type TProperty = 'body' | 'header' | 'params' | 'query';
 
-type TGetSchema = <T extends AnyObject | Maybe<AnyObject>>(schema: ObjectSchema<T>) => ObjectSchema<T>;
+type TGetSchema = <T extends Maybe<AnyObject>>(schema: ObjectSchema<T>) => ObjectSchema<T>
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type TAllSchemas = Record<TProperty, ObjectSchema<any>>
+type TAllSchemas = Record<TProperty, ObjectSchema<any>>;
 
-type TGetAllSchemas = (getschema: TGetSchema) => Partial<TAllSchemas>
+type TGetAllSchemas = (getSchema: TGetSchema) => Partial<TAllSchemas>;
 
 type TValidation = (getAllSchemas: TGetAllSchemas) => RequestHandler;
 
 export const validation: TValidation = (getAllSchemas) => async (req, res, next) => {
-
   const schemas = getAllSchemas((schema) => schema);
 
-  const errorsResult: Record<string, Record<string, string>> = {
 
-  };
+  const errorsResult: Record<string, Record<string, string>> = {};
 
   Object.entries(schemas).forEach(([key, schema]) => {
-    try{
+    try {
       schema.validateSync(req[key as TProperty], { abortEarly: false });
-    }catch(err){
+    } catch (err) {
       const yupError = err as ValidationError;
       const errors: Record<string, string> = {};
-    
-      yupError.inner.forEach( error => {
-    
-        if(error.path === undefined) return;
+
+      yupError.inner.forEach(error => {
+        if (error.path === undefined) return;
         errors[error.path] = error.message;
-           
       });
-      
+
       errorsResult[key] = errors;
-      
     }
   });
-  
-  if(Object.entries(errorsResult).length === 0) {
+
+
+  if (Object.entries(errorsResult).length === 0) {
     return next();
   } else {
-    return res.status(StatusCodes.BAD_REQUEST).json({errors: errorsResult});
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors: errorsResult });
   }
 };
